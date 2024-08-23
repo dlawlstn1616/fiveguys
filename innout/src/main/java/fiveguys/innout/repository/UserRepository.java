e71e21b6d517a5512203723f69168aa16c681662
김욱진 <dnrwls9115@naver.com>
2024-08-23 13:37:52 +0900

    repository 생성

---
+++ innout/src/main/java/fiveguys/innout/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package fiveguys.innout.repository;
+
+import fiveguys.innout.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
