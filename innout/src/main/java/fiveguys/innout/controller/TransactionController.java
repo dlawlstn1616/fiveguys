f22eb0a2cd59ec3cff5bd9bfbbbcba5f5cd4c731
윤수빈 <soobindy1@gmail.com>
2024-08-23 17:17:43 +0900

    test test

--- innout/src/main/java/fiveguys/innout/controller/TransactionController.java
+++ innout/src/main/java/fiveguys/innout/controller/TransactionController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/innout")
 public class TransactionController {
-
+//22
     @Autowired
     private TransactionService transactionService;
 
