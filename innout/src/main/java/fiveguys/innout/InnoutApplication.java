878ed9073b239c728796ee9ddb0964e34de4bc6b
김욱진 <dnrwls9115@naver.com>
2024-08-23 09:31:22 +0900

    spring initializar

---
+++ innout/src/main/java/fiveguys/innout/InnoutApplication.java
@@ -0,0 +1,13 @@
+package fiveguys.innout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoutApplication.class, args);
+	}
+
+}
