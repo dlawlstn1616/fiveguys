4e0fa59c1c5be76c232e1c201ec6fbb4a49da304
김욱진 <dnrwls9115@naver.com>
2024-08-29 10:33:06 +0900

    ã소비평균 서비스

--- innout/src/main/java/fiveguys/innout/repository/TransactionRepository.java
+++ innout/src/main/java/fiveguys/innout/repository/TransactionRepository.java
@@ -3,5 +3,10 @@ package fiveguys.innout.repository;
 import fiveguys.innout.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByUserEmail(String email);
+
+    List<Transaction> findByUserId(Long userid);
 }
